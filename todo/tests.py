12dccfc58256f5c711844306acc0c1005f55ed53
1F10220180 <s1F102201807@iniad.org>
2023-07-02 11:34:45 +0900

    エラー箇所の修正

--- todo/tests.py
+++ todo/tests.py
@@ -108,7 +108,7 @@ class TodoViewTestCase(TestCase):
 
 
     def test_detail_get_success(self):
-        task = Task(title='task1', due_to=timezone.make_aware(datetime(2023, 7, 1)))
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
         task.save()
         client = Client()
         response = client.get('/{}/'.format(task.pk))
